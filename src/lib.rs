de50898be7e2fb4a3837c43febde8cb15be4816b
ukiyoyo <13175394+ukiyoyo@users.noreply.github.com>
2021-02-01 16:28:40 +0900

    Add tests

--- src/lib.rs
+++ src/lib.rs
@@ -105,3 +105,55 @@ impl Client {
         Ok(devices)
     }
 }
+
+#[cfg(test)]
+mod test_remo_client_rs {
+    use super::*;
+    use std::fs;
+
+    #[test]
+    fn test_user_deserialize() {
+        let json = fs::read_to_string("samples/user.json").unwrap();
+        let user: User = serde_json::from_str(&json).unwrap();
+
+        assert_eq!(user.id, "0123456789abcdefg");
+        assert_eq!(user.nickname, "Test user");
+    }
+
+    #[test]
+    fn test_devices_deserialize() {
+        let json = fs::read_to_string("samples/devices.json").unwrap();
+        let device: Vec<Device> = serde_json::from_str(&json).unwrap();
+
+        assert_eq!(device[0].id, "my_nature_remo_3");
+        assert_eq!(device[0].name, "  Remo 3       ");
+        assert_eq!(device[0].temperature_offset, 0);
+        assert_eq!(device[0].humidity_offset, 0);
+        assert_eq!(device[0].created_at, "2020-12-20T03:06:40Z");
+        assert_eq!(device[0].updated_at, "2021-01-31T15:37:25Z");
+        assert_eq!(device[0].firmware_version, "Remo/1.2.14");
+        assert_eq!(device[0].mac_address, "a1:b2:c3:d4:e5:f6");
+        assert_eq!(device[0].bt_mac_address, "a1:b2:c3:d4:e5:f7");
+        assert_eq!(device[0].serial_number, "1W0123456789");
+        assert_eq!(device[0].newest_events.hu.val, 43.0);
+        assert_eq!(
+            device[0].newest_events.hu.created_at,
+            "2021-01-31T16:46:08Z"
+        );
+        assert_eq!(device[0].newest_events.il.val, 5.0);
+        assert_eq!(
+            device[0].newest_events.il.created_at,
+            "2021-01-31T16:46:28Z"
+        );
+        assert_eq!(device[0].newest_events.mo.val, 1.0);
+        assert_eq!(
+            device[0].newest_events.mo.created_at,
+            "2021-01-31T16:06:08Z"
+        );
+        assert_eq!(device[0].newest_events.te.val, 19.1);
+        assert_eq!(
+            device[0].newest_events.te.created_at,
+            "2021-01-31T16:58:09Z"
+        );
+    }
+}
